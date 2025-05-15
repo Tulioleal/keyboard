41b52f2f5f36b4935d800af0b2f89cd581610041
Tulioleal <tuliolealdev@gmail.com>
2025-05-15 17:31:45 -0300

    feat: update keyboard key symbols with new icons
    - Replaced F1 to F12 key symbols with corresponding icons (sun_low, sun_high, layout, search, microphone, moon, rewind, play_pause, forward, mute, volume_down, volume_up).
    - Updated delete key symbol with a new delete icon.
    - Added tab and caps lock icons.
    - Introduced shift, control, and option icons.
    - Implemented arrow key icons (left, up, down, right).
    - Added new SVG icons for various functionalities in the keyboard layout.

--- app/const/Keys.ts
+++ app/const/Keys.ts
@@ -1,4 +1,5 @@
 import { ColSpanClass, Key, RowSpanClass } from "../interfaces/Key";
+import * as ICONS from "@/app/icons/index"
 
 export const KEYS = [
   // First Row
@@ -8,62 +9,62 @@ export const KEYS = [
     rowSpan: RowSpanClass.TWO,
   },
   {
-    symbols: { symbol1: "F1", symbol2: "" },
+    symbols: { symbol1: "F1", symbol2: ICONS.sun_low },
     colSpan: ColSpanClass.FOUR,
     rowSpan: RowSpanClass.TWO,
   },
   {
-    symbols: { symbol1: "F2", symbol2: "" },
+    symbols: { symbol1: "F2", symbol2: ICONS.sun_high },
     colSpan: ColSpanClass.FOUR,
     rowSpan: RowSpanClass.TWO,
   },
   {
-    symbols: { symbol1: "F3", symbol2: "" },
+    symbols: { symbol1: "F3", symbol2: ICONS.layout },
     colSpan: ColSpanClass.FOUR,
     rowSpan: RowSpanClass.TWO,
   },
   {
-    symbols: { symbol1: "F4", symbol2: "" },
+    symbols: { symbol1: "F4", symbol2: ICONS.search },
     colSpan: ColSpanClass.FOUR,
     rowSpan: RowSpanClass.TWO,
   },
   {
-    symbols: { symbol1: "F5", symbol2: "" },
+    symbols: { symbol1: "F5", symbol2: ICONS.microphone },
     colSpan: ColSpanClass.FOUR,
     rowSpan: RowSpanClass.TWO,
   },
   {
-    symbols: { symbol1: "F6", symbol2: "" },
+    symbols: { symbol1: "F6", symbol2: ICONS.moon },
     colSpan: ColSpanClass.FOUR,
     rowSpan: RowSpanClass.TWO,
   },
   {
-    symbols: { symbol1: "F7", symbol2: "" },
+    symbols: { symbol1: "F7", symbol2: ICONS.rewind },
     colSpan: ColSpanClass.FOUR,
     rowSpan: RowSpanClass.TWO,
   },
   {
-    symbols: { symbol1: "F8", symbol2: "" },
+    symbols: { symbol1: "F8", symbol2: ICONS.play_pause },
     colSpan: ColSpanClass.FOUR,
     rowSpan: RowSpanClass.TWO,
   },
   {
-    symbols: { symbol1: "F9", symbol2: "" },
+    symbols: { symbol1: "F9", symbol2: ICONS.forward },
     colSpan: ColSpanClass.FOUR,
     rowSpan: RowSpanClass.TWO,
   },
   {
-    symbols: { symbol1: "F10", symbol2: "" },
+    symbols: { symbol1: "F10", symbol2: ICONS.mute },
     colSpan: ColSpanClass.FOUR,
     rowSpan: RowSpanClass.TWO,
   },
   {
-    symbols: { symbol1: "F11", symbol2: "" },
+    symbols: { symbol1: "F11", symbol2: ICONS.volume_down },
     colSpan: ColSpanClass.FOUR,
     rowSpan: RowSpanClass.TWO,
   },
   {
-    symbols: { symbol1: "F12", symbol2: "" },
+    symbols: { symbol1: "F12", symbol2: ICONS.volume_up },
     colSpan: ColSpanClass.FOUR,
     rowSpan: RowSpanClass.TWO,
   },
@@ -139,13 +140,13 @@ export const KEYS = [
     rowSpan: RowSpanClass.TWO,
   },
   {
-    symbols: { symbol1: "delete", symbol2: "=" },
+    symbols: { symbol1: ICONS.delete_icon },
     colSpan: ColSpanClass.SIX,
     rowSpan: RowSpanClass.TWO,
   },
   // Third Row
   {
-    symbols: { symbol1: "tab" },
+    symbols: { symbol1: ICONS.tab },
     colSpan: ColSpanClass.SIX,
     rowSpan: RowSpanClass.TWO,
   },
@@ -217,7 +218,7 @@ export const KEYS = [
   },
   // fourth row
   {
-    symbols: { symbol1: "caps" },
+    symbols: { symbol1: ICONS.caps },
     colSpan: ColSpanClass.SEVEN,
     rowSpan: RowSpanClass.TWO,
   },
@@ -283,7 +284,7 @@ export const KEYS = [
   },
   // Fifth row
   {
-    symbols: { symbol1: "Shift" },
+    symbols: { symbol1: ICONS.shift },
     colSpan: ColSpanClass.FIVE,
     rowSpan: RowSpanClass.TWO,
   },
@@ -343,23 +344,23 @@ export const KEYS = [
     rowSpan: RowSpanClass.TWO,
   },
   {
-    symbols: { symbol1: "Shift" },
+    symbols: { symbol1: ICONS.shift },
     colSpan: ColSpanClass.NINE,
     rowSpan: RowSpanClass.TWO,
   },
   // Sixth row
   {
-    symbols: { symbol1: "fn" },
+    symbols: { symbol1: ICONS.globe, symbol2: "fn" },
     colSpan: ColSpanClass.FOUR,
     rowSpan: RowSpanClass.TWO,
   },
   {
-    symbols: { symbol1: "control" },
+    symbols: { symbol1: "control", symbol2: ICONS.control },
     colSpan: ColSpanClass.FOUR,
     rowSpan: RowSpanClass.TWO,
   },
   {
-    symbols: { symbol1: "option" },
+    symbols: { symbol1: "option", symbol2: ICONS.option },
     colSpan: ColSpanClass.FOUR,
     rowSpan: RowSpanClass.TWO,
   },
@@ -379,30 +380,30 @@ export const KEYS = [
     rowSpan: RowSpanClass.TWO,
   },
   {
-    symbols: { symbol1: "option" },
+    symbols: { symbol1: "option", symbol2: ICONS.option },
     colSpan: ColSpanClass.FOUR,
     rowSpan: RowSpanClass.TWO,
   },
   {
-    symbols: { symbol1: "left" },
+    symbols: { symbol1: ICONS.left_key },
     colSpan: ColSpanClass.FOUR,
     rowSpan: RowSpanClass.ONE,
     additionalClasses: "col-start-47 col-end-51 row-start-12",
   },
   {
-    symbols: { symbol1: "top" },
+    symbols: { symbol1: ICONS.up_key },
     colSpan: ColSpanClass.FOUR,
     rowSpan: RowSpanClass.ONE,
     additionalClasses: "col-start-51",
   },
   {
-    symbols: { symbol1: "bottom" },
+    symbols: { symbol1: ICONS.down_key },
     colSpan: ColSpanClass.FOUR,
     rowSpan: RowSpanClass.ONE,
     additionalClasses: "col-start-51",
   },
   {
-    symbols: { symbol1: "right" },
+    symbols: { symbol1: ICONS.right_key },
     colSpan: ColSpanClass.FOUR,
     rowSpan: RowSpanClass.ONE,
     additionalClasses: "col-start-55",
